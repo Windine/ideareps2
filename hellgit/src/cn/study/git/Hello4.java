dafc601deb2c51c02a2f15b979f8217970f4238e
Windine <QEY62392795F@163.com>
2022-07-01 16:58:18 +0800

    add fourth project, version 4

---
+++ hellgit/src/cn/study/git/Hello4.java
@@ -0,0 +1,13 @@
+package cn.study.git;
+
+/**
+ * @ClassName Hello4
+ * @Description TODO
+ * @Date 2022/7/1 16:52
+ * @Create By    Windine
+ */
+public class Hello4 {
+    public static void main(String[] args) {
+        System.out.println("this is fourth project, version 1");
+    }
+}
