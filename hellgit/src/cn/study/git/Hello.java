85da0b038fcc702fd34c70e5e9efc7050ead3622
Windine <QEY62392795F@163.com>
2022-07-01 14:54:53 +0800

    use idea commit,version 3

--- hellgit/src/cn/study/git/Hello.java
+++ hellgit/src/cn/study/git/Hello.java
@@ -10,5 +10,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git.Version.1");
         System.out.println("this is version 2");
+        System.out.println("this is version 3");
     }
 }
