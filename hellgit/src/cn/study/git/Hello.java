a0b84cc931ad863f1291cf4893295feb9abb62a8
Windine <QEY62392795F@163.com>
2022-07-01 14:26:45 +0800

    新增内容，version 1

---
+++ hellgit/src/cn/study/git/Hello.java
@@ -0,0 +1,13 @@
+package cn.study.git;
+
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Date 2022/7/1 14:20
+ * @Create By    Windine
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git.Version.1");
+    }
+}
