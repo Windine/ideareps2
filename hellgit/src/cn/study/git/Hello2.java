68d00ba0746c991255fcb5575697f580c04f339c
Windine <QEY62392795F@163.com>
2022-07-01 16:42:25 +0800

    add second project, version 1

---
+++ hellgit/src/cn/study/git/Hello2.java
@@ -0,0 +1,13 @@
+package cn.study.git;
+
+/**
+ * @ClassName Hello2
+ * @Description TODO
+ * @Date 2022/7/1 16:30
+ * @Create By    Windine
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("this is the second project");
+    }
+}
